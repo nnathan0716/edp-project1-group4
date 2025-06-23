9c9b55c0a1e4ddccfd93cf10b5c77b20ec4f00fb
Nitin Nathan <nnathan2@travelers.com>
2025-06-23 15:25:58 -0400

    Updated html to have relevant fields

--- vehicle.js
+++ vehicle.js
@@ -1,1 +1,24 @@
-let 
+let nameH1;
+let drivers;
+const baseUrl = `http://localhost:9001/api`;
+
+addEventListener('DOMContentLoaded', () => {
+    nameH1 = document.querySelector('h1#name');
+    drivers = document.querySelector('#drivers>ul');
+    const sp = new URLSearchParams(window.location.search)
+    const id = sp.get('id')
+    console.log(id)
+    getVehicle(id)
+});
+
+async function getVehicle(id) {
+    let vehicle;
+    try {
+        vehicle = await fetchVehicle(id)
+        vehicle.drivers = await fetchDrivers(id)
+      }
+      catch (ex) {
+        console.error(`Error reading character ${id} data.`, ex.message);
+      }
+      renderVehicle(vehicle);
+}
