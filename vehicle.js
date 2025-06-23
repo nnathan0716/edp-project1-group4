bcdf97232a83c6ea15d461c733e44855f1da2d38
Nitin Nathan <nnathan2@travelers.com>
2025-06-23 15:36:08 -0400

    Vehicle name shows up

--- vehicle.js
+++ vehicle.js
@@ -16,9 +16,27 @@ async function getVehicle(id) {
     try {
         vehicle = await fetchVehicle(id)
         vehicle.drivers = await fetchDrivers(id)
-      }
-      catch (ex) {
+        console.log(vehicle)
+        console.log(vehicle.drivers)
+    }
+    catch (ex) {
         console.error(`Error reading character ${id} data.`, ex.message);
-      }
-      renderVehicle(vehicle);
+    }
+        renderVehicle(vehicle);
+}
+
+async function fetchVehicle(id) {
+    let vehicleUrl = `${baseUrl}/vehicles/${id}`;
+    return await fetch(vehicleUrl)
+      .then(res => res.json())
+}
+async function fetchDrivers(id) {
+    let driverUrl = `${baseUrl}/vehicles/${id}/characters`;
+    return await fetch(driverUrl)
+      .then(res => res.json())
+}
+
+const renderVehicle = vehicle => {
+    document.title = `SWAPI - ${vehicle?.vehicle_class}`;  // Just to make the browser tab say their name (yoinked from character.js, didn't know you could do this)
+    nameH1.textContent = vehicle?.vehicle_class
 }
